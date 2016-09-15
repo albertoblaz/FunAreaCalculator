ece24671560c214945a160d8a1f38f3bfa0b6374
Alberto Blázquez <hola@albertoblazquez.net>
2016-09-15 18:47:53 +0200

    Configure webpack server

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname,
+    filename: "dist/bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime', 'transform-exponentiation-operator'],
+        }
+      }
+  ]
+  }
+};
